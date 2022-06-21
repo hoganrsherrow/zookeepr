b8295b624fea02638001ea5518a3404534727fc7
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-06-21 18:59:55 -0500

    added 404 error and findById

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -40,7 +41,10 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
 }
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if(req.query) {
@@ -48,6 +52,15 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
-app.listen(3001, () => {
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if(result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+    
+});
+app.listen(PORT, () => {
     console.log('API server now on port 3001');
 });
